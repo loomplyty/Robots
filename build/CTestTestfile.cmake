1c8f2ef9f0651137f9f9a917a5f7cb940703d703
TianYuan <tianyuan_123abc@sjtu.edu.cn>
2017-04-18 15:34:32 +0800

    FirstForXIII

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hex/Desktop/XIII/Robots
+# Build directory: /home/hex/Desktop/XIII/Robots/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(test_simple_walk "/home/hex/Desktop/XIII/Robots/build/bin/test_simple_walk")
